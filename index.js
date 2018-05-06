04e965fc9d49f908988023e31579f586ff4c8349
Ray Deck <ray@raydeck.com>
2018-05-06 09:56:01 -0400

    New JS/TS code

--- index.js
+++ index.js
@@ -1,36 +1,68 @@
-import { NativeModules } from 'react-native';
-class RNCoreMLClass {
-}
+var __awaiter =
+  (this && this.__awaiter) ||
+  function(thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function(resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : new P(function(resolve) {
+              resolve(result.value);
+            }).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+import { NativeModules } from "react-native";
+class RNCoreMLClass {}
 const RNCoreML = NativeModules.RNCoreML;
-const compileModel = (sourcepath) => {
-    return RNCoreML.compileModel(sourcepath);
+const compileModel = sourcepath =>
+  __awaiter(this, void 0, void 0, function*() {
+    return yield RNCoreML.compileModel(sourcepath);
+  });
+const classifyImage = (imagePath, modelPath) =>
+  __awaiter(this, void 0, void 0, function*() {
+    return yield RNCoreML.classifyImageWithModel(imagePath, modelPath);
+  });
+const getTopResult = arr => {
+  return arr[0];
 };
-const classifyImage = (imagePath, modelPath) => {
-    return RNCoreML.classifyImageWithModel(imagePath, modelPath);
+const getTopFiveResults = arr => {
+  return arr.slice(0, 4);
 };
-const getTopResult = (arr) => {
-    let ret = new Promise((resolve, reject) => {
-        resolve(arr[0]);
-    });
-    return ret;
-};
-const getTopFiveResults = (arr) => {
-    let ret = new Promise((resolve, reject) => {
-        resolve(arr.slice(0, 4));
-    });
-    return ret;
-};
-const classifyTopFive = (imagePath, modelPath) => {
-    return RNCoreML.classifyImageWithModel(imagePath, modelPath)
-        .then(getTopFiveResults);
-};
-const classifyTopValue = (imagePath, modelPath) => {
-    return RNCoreML.classifyImageWithModel(imagePath, modelPath).then(getTopResult);
-};
+const classifyTopFive = (imagePath, modelPath) =>
+  __awaiter(this, void 0, void 0, function*() {
+    const arr = yield RNCoreML.classifyImageWithModel(imagePath, modelPath);
+    return getTopFiveResults(arr);
+  });
+const classifyTopValue = (imagePath, modelPath) =>
+  __awaiter(this, void 0, void 0, function*() {
+    const arr = yield RNCoreML.classifyImageWithModel(imagePath, modelPath);
+    return getTopResult(arr);
+  });
+const predict = (dictionary, modelPath) =>
+  __awaiter(this, void 0, void 0, function*() {
+    const obj = yield RNCoreML.predictFromDataWithModel(dictionary, modelPath);
+    return obj;
+  });
 export default {
-    "compileModel": compileModel,
-    "classifyImage": classifyImage,
-    "classifyTopFive": classifyTopFive,
-    "classifyTopValue": classifyTopValue
+  compileModel: compileModel,
+  classifyImage: classifyImage,
+  classifyTopFive: classifyTopFive,
+  classifyTopValue: classifyTopValue,
+  predict: predict
 };
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,41 +1,44 @@
-
-import {
-  NativeModules
-} from 'react-native';
+import { NativeModules } from "react-native";
 class RNCoreMLClass {
-  compileModel: (sourcepath:string) => Promise<string>;
-  classifyImageWithModel: (imagepath:string, modelpath:string) => Promise<Array<Object>>;
-  predictFromDataWithModel: (data:object, modelpath: string) => Promise<object>
+  compileModel: (sourcepath: string) => Promise<string>;
+  classifyImageWithModel: (
+    imagepath: string,
+    modelpath: string
+  ) => Promise<Array<Object>>;
+  predictFromDataWithModel: (
+    data: object,
+    modelpath: string
+  ) => Promise<object>;
 }
-const RNCoreML:RNCoreMLClass = NativeModules.RNCoreML;
-const compileModel = (sourcepath) => {
-  return RNCoreML.compileModel(sourcepath)
-}
-const classifyImage = (imagePath:string, modelPath:string): Promise<object> => {
-  return RNCoreML.classifyImageWithModel(imagePath, modelPath)
-}
-const getTopResult = (arr:Array<object>):Promise<Object> => {
-  let ret = new Promise<object>((resolve, reject) => {
-    resolve(arr[0])  ;  
-  });
-  return ret; 
-}
-const getTopFiveResults = (arr:Array<object>):Promise<Array<object>> => {
-  let ret = new Promise<Array<object>>((resolve, reject) => {
-    resolve(arr.slice(0,4))  ;  
-  });
-  return ret;
-}
-const classifyTopFive = (imagePath:string, modelPath:string):Promise<Array<object>> => {
-  return RNCoreML.classifyImageWithModel(imagePath, modelPath)
-    .then(getTopFiveResults)
+const RNCoreML: RNCoreMLClass = NativeModules.RNCoreML;
+const compileModel = async sourcepath => {
+  return await RNCoreML.compileModel(sourcepath);
 };
-const classifyTopValue = (imagePath, modelPath) => {
-  return RNCoreML.classifyImageWithModel(imagePath, modelPath).then(getTopResult); 
-}
+const classifyImage = async (imagePath, modelPath) => {
+  return await RNCoreML.classifyImageWithModel(imagePath, modelPath);
+};
+const getTopResult = arr => {
+  return arr[0];
+};
+const getTopFiveResults = arr => {
+  return arr.slice(0, 4);
+};
+const classifyTopFive = async (imagePath, modelPath) => {
+  const arr = await RNCoreML.classifyImageWithModel(imagePath, modelPath);
+  return getTopFiveResults(arr);
+};
+const classifyTopValue = async (imagePath, modelPath) => {
+  const arr = await RNCoreML.classifyImageWithModel(imagePath, modelPath);
+  return getTopResult(arr);
+};
+const predict = async (dictionary, modelPath) => {
+  const obj = await RNCoreML.predictFromDataWithModel(dictionary, modelPath);
+  return obj;
+};
 export default {
-  "compileModel": compileModel,
-  "classifyImage": classifyImage,
-  "classifyTopFive": classifyTopFive,
-  "classifyTopValue": classifyTopValue
-}
+  compileModel: compileModel,
+  classifyImage: classifyImage,
+  classifyTopFive: classifyTopFive,
+  classifyTopValue: classifyTopValue,
+  predict: predict
+};
