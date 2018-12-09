24d08eab08e776622743f99d76585ccd6673c7a4
Ray Deck <ray@raydeck.com>
2018-12-09 13:30:35 -0500

    Compiled code

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { NativeModules } from "react-native";
+import { NativeModules, Image } from "react-native";
+const { resolveAssetSource } = Image;
 class RNCoreMLClass {
 }
 const RNCoreML = NativeModules.RNCoreML;
+const { mainBundlePath, mainBundleURL } = RNCoreML;
+const fixURL = sourcepath => {
+    if (typeof sourcepath === "string") {
+        if (sourcepath.includes("/"))
+            return sourcepath;
+        else
+            return mainBundleURL + "/" + sourcepath;
+    }
+    if (sourcepath.uri) {
+        return sourcepath.uri;
+    }
+    const { uri } = resolveAssetSource(sourcepath);
+    return uri;
+};
 const compileModel = (sourcepath) => __awaiter(this, void 0, void 0, function* () {
-    return yield RNCoreML.compileModel(sourcepath);
+    return yield RNCoreML.compileModel(fixURL(sourcepath));
 });
 const classifyImage = (imagePath, modelPath) => __awaiter(this, void 0, void 0, function* () {
-    return yield RNCoreML.classifyImageWithModel(imagePath, modelPath);
+    return yield RNCoreML.classifyImageWithModel(fixURL(imagePath), fixURL(modelPath));
 });
 const getTopResult = arr => {
     return arr[0];
@@ -23,15 +38,15 @@ const getTopFiveResults = arr => {
     return arr.slice(0, 4);
 };
 const classifyTopFive = (imagePath, modelPath) => __awaiter(this, void 0, void 0, function* () {
-    const arr = yield RNCoreML.classifyImageWithModel(imagePath, modelPath);
+    const arr = classifyImage(imagePath, modelPath);
     return getTopFiveResults(arr);
 });
 const classifyTopValue = (imagePath, modelPath) => __awaiter(this, void 0, void 0, function* () {
-    const arr = yield RNCoreML.classifyImageWithModel(imagePath, modelPath);
+    const arr = classifyImage(imagePath, modelPath);
     return getTopResult(arr);
 });
 const predict = (dictionary, modelPath) => __awaiter(this, void 0, void 0, function* () {
-    const obj = yield RNCoreML.predictFromDataWithModel(dictionary, modelPath);
+    const obj = yield RNCoreML.predictFromDataWithModel(dictionary, fixURL(modelPath));
     return obj;
 });
 const saveMultiArray = (key, savePath) => __awaiter(this, void 0, void 0, function* () {
@@ -46,7 +61,9 @@ export default {
     classifyTopFive,
     classifyTopValue,
     predict,
-    saveMultiArray
+    saveMultiArray,
+    mainBundlePath,
+    mainBundleURL
 };
-export { compileModel, classifyImage, classifyTopFive, classifyTopValue, predict, saveMultiArray };
+export { compileModel, classifyImage, classifyTopFive, classifyTopValue, predict, saveMultiArray, mainBundlePath, mainBundleURL };
 //# sourceMappingURL=index.js.map
